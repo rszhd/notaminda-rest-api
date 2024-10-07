9c4b1e472da43468816b364962f7e49c9735560f
kapitan <harithzahid.dev@gmail.com>
2024-10-07 14:33:51 +0800

    Refactor others

--- api/__init__.py
+++
@@ -1,1 +0,0 @@
-from .utils import chat_stream
--- api/urls.py
+++ api/urls.py
@@ -7,8 +7,7 @@ from .views import (
   PublicMindMapViewSet,
   register_user,
   login_user,
-  check_ai_api_key,
-  chat_view
+  verify_ai_key,
 )
 
 router = DefaultRouter()
@@ -21,15 +20,14 @@ urlpatterns = [
     path('', include(router.urls)),
     path('register/', register_user, name='register'),
     path('login/', login_user, name='login'),
-    path('chat/', chat_view, name='chat_view'),
-    path('check-ai-api-key/', check_ai_api_key, name='check_ai_api_key'),
+    path('verify-ai-key/', verify_ai_key, name='verify_ai_key'),
     path(
-      'nodes/<str:pk>/auto_generate_children/',
+      'nodes/<str:pk>/auto-generate-children/',
       NodeViewSet.as_view({'post': 'auto_generate_children'}),
       name='node-auto-generate-children'
     ),
     path(
-      'nodes/<str:pk>/auto_generate_note/',
+      'nodes/<str:pk>/auto-generate-note/',
       NodeViewSet.as_view({'post': 'auto_generate_note'}),
       name='node-auto-generate-note'
     ),
