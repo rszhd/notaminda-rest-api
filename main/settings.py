9a5fc60ea10c6726385ae287b19e3d7b239aee2c
kapitan <harithzahid.dev@gmail.com>
2024-10-07 20:10:15 +0800

    load_dotenv(verbose=True, override=True)

--- main/settings.py
+++ main/settings.py
@@ -15,7 +15,8 @@ import os
 from dotenv import load_dotenv
 from datetime import timedelta
 
-load_dotenv()
+load_dotenv(verbose=True, override=True)
+print(os.environ.get('DB_NAME'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
