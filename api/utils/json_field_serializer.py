c2f6d29acb4f14ef0ec37a0d17394ee319ed05ce
kapitan <harithzahid.dev@gmail.com>
2024-10-07 14:10:06 +0800

    Add utils

---
+++ api/utils/json_field_serializer.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+import json
+
+class JSONFieldSerializer(serializers.Field):
+    def to_representation(self, value):
+        if value is None:
+            return None
+        if isinstance(value, str):
+            try:
+                return json.loads(value)
+            except json.JSONDecodeError:
+                return None
+        return value
+
+    def to_internal_value(self, data):
+        if data is None:
+            return None
+        if isinstance(data, str):
+            try:
+                return json.loads(data)
+            except json.JSONDecodeError:
+                raise serializers.ValidationError("Invalid JSON data")
+        return json.dumps(data)
